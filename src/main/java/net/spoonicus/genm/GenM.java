e03b9ae2f0982426274fe6250c791df8afdb0c02
SpoonicusIV <amoore203657@gmail.com>
2025-05-16 20:35:04 -0400

    Testing to see if commiting works

--- src/main/java/net/spoonicus/genm/GenM.java
+++ src/main/java/net/spoonicus/genm/GenM.java
@@ -15,6 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+// Test commit
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(GenM.MOD_ID)
 public class GenM
